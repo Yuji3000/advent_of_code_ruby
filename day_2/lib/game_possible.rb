e1083825c561971cca9eabc1e5666d4e2b1ec26d
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-10-04 00:54:23 -0600

    WIP

--- day_2/lib/game_possible.rb
+++ day_2/lib/game_possible.rb
@@ -1,6 +1,27 @@
-def read_and_split
-  file_content = File.read(File.join(__dir__, "../input.txt")).split("\n")
-  file_content
-end
+class GamePossible
+  attr_reader :id_counter,
+              :file_content
+  def initialize
+    @id_counter = 0
+    @file_content = File.read(File.join(__dir__, "../input.txt")).split("\n")
+  end
 
-puts read_and_split
+  def possible
+    @file_content.map do |line|
+      split_array = line.split(/[:,;]/)
+      split_array.select do |a|
+        temp_id = split_array[0].chars.select { |c| c.match?(/[[:digit:]]/) }.first
+        require 'pry'; binding.pry
+
+      end
+    end
+  end
+
+
+end
+# 12 red cubes
+# 13 green cubes
+# 14 blue cubes
+
+game = GamePossible.new
+puts game.possible
